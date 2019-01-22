0ab0a2187eaf6294a194a782baa8773ec66bebf8
ybbt <yorik_boss@ukr.net>
2019-01-22 06:39:53 +0200

    полностью готова структура документа и основной внешний вид. Осталось настроить все отступьі, вставить мелкие єлементьі оформления и настроить меню перехода по зонам

--- 2019_01_19_Exam_2/js/script.js
+++ 2019_01_19_Exam_2/js/script.js
@@ -14,6 +14,23 @@ $(document).ready(function(){
 	
 });
 
+$(document).ready(function(){
+			
+	$('._mainContainer').slick({
+		slidesToShow: 3,
+		slidesToScroll:1,
+		// arrows: true,
+		// // fade: true,
+		dots: true,
+		// // autoplay: true,
+		// // autoplaySpeed: 4000,
+		// centerMode: true,
+		prevArrow: '<img src="img/service_img/arrow_left.png" class="arr-prev">',
+		nextArrow: '<img src="img/service_img/arrow_right.png" class="arr-next">',
+	});
+	
+});
+
 function loadMap() {
 	// alert('ho-ho-ho');
 	var loc = {lat:40.67, lng: -73.94};
@@ -22,6 +39,7 @@ function loadMap() {
 		center:loc, 
 		disableDefaultUI: true
 	});
+	
 };
 
 $(function(){
